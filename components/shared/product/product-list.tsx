37f6b834b7c45b5530aa54f39bea6e3a783b5c48
= <=>
2025-01-29 13:11:44 -0500

    add product type

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,12 +1,26 @@
 import ProductCard from "./product-card";
 
-const ProductList = ({ data, title, limit }: { data: any; title?: string; limit?: number }) => {
+export type Product = {
+    name: string;
+    slug: string;
+    category: string;
+    description: string;
+    images: string[];
+    price: number;
+    brand: string;
+    rating: number;
+    numReviews: number;
+    stock: number;
+    isFeatured: boolean;
+    banner: string | null;
+}
+const ProductList = ({ data, title, limit }: { data: Product[]; title?: string; limit?: number }) => {
     const limitedData = limit ? data.slice(0, limit) : data
     return (<div className="my-10">
         <h2 className="h2-bold mb-4">{title}</h2>
         {data.length > 0 ? (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {limitedData.map((product: any) => {
+                {limitedData.map((product: Product) => {
                     return (
                         <ProductCard key={product.slug} product={product} />
                     )
