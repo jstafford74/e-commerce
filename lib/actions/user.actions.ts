bd2950bfe497c05057d1ea0ac9e3a0aeea46f663
= <=>
2025-02-05 14:06:16 -0500

    user action sigup function fix

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,11 @@
 "use server";
-import { signInFormSchema } from "../validators";
+import { signInFormSchema, signUpFormSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { hash } from "../encrypt";
+import prisma from "@/db/prisma";
+import { formatError } from "../utils";
+
 // Sign in the user with credentials
 export async function signInWithCredentials(
   prevState: unknown,
@@ -23,4 +27,39 @@ export async function signInWithCredentials(
 // Sign the user out
 export async function signOutUser() {
   await signOut();
+}
+
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      confirmPassword: formData.get("confirmPassword"),
+    });
+
+    const plainPassword = user.password;
+
+    user.password = await hash(user.password);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn("credentials", {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return { success: true, message: "User registered successfully" };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return { success: false, message: formatError(error) };
+  }
 }
