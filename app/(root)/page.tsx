0000050b9f55b0aa617b57b4fba9e82852bbfc6a
= <=>
2025-02-23 13:03:57 -0500

    add filtering and update images

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,30 @@
 import ProductList from "@/components/shared/product/product-list";
-
-import { getLatestProducts } from "@/lib/actions/product.actions";
-
-// const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from "@/lib/actions/product.actions";
+import ProductCarousel from "@/components/shared/product/product-carousel";
+import ViewAllProductsButton from "@/components/view-all-products-button";
+import IconBoxes from "@/components/icon-boxes";
+// import DealCountdown from "@/components/deal-countdown";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList
         data={latestProducts}
-        title="Most Recent Arrivals"
-        limit={4}
+        title="HORSESHOE HATS HAVE ARRIVED"
+        limit={8}
       />
+      <ViewAllProductsButton />
+      {/* <DealCountdown /> */}
+      <IconBoxes />
     </>
   );
 };
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,15 +1,17 @@
-import Menu from "./menu";
 import Image from "next/image";
 import Link from "next/link";
-
 import { APP_NAME } from "@/lib/constants";
+import Menu from "./menu";
+import CategoryDrawer from "./category-drawer";
+import Search from "./search";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className="flex-start">
+          <CategoryDrawer />
+          <Link href="/" className="flex-start ml-4">
             <Image
               src="/images/nobafrog_image.svg"
               alt={`${APP_NAME} logo`}
@@ -17,12 +19,14 @@ const Header = () => {
               width={48}
               priority={true}
             />
-            <span className="hidden lg:block font-bold text-2xl ml-2">
+            <span className="hidden lg:block font-bold text-2xl ml-3">
               {APP_NAME}
             </span>
           </Link>
         </div>
-
+        <div className="hidden md:block">
+          <Search />
+        </div>
         <Menu />
       </div>
     </header>
