2a3c6983de4ee07e1b36fd37928f5a145a6f466e
= <=>
2025-02-23 10:53:24 -0500

    upgrade logo

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -11,7 +11,7 @@ const Header = () => {
         <div className="flex-start">
           <Link href="/" className="flex-start">
             <Image
-              src="/images/logo.svg"
+              src="/images/nobafrog_image.svg"
               alt={`${APP_NAME} logo`}
               height={48}
               width={48}
